abf92a465d1e969b38da87a3dff8c14db3cb701f
laurindocbenjamim <rocketmc2009@gmail.com>
2026-02-03 03:27:13 +0000

    welcome route

--- auth-backend/app/main.py
+++ auth-backend/app/main.py
@@ -31,6 +31,6 @@ app.include_router(users.router, prefix="/users", tags=["Users"])
 async def root():
     return {"message": f"Welcome to {settings.PROJECT_NAME}"}
 
-@app.get("welcome")
+@app.get("/welcome")
 async def welcome():
     return HTMLResponse("<h1>Welcome to {settings.PROJECT_NAME}</h1>")
